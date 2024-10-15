707de7ef55e458ff739cbbf73b7f4836c940b1cf
Leo Velez <leonardo.velez@kaplan.edu>
2024-10-15 16:45:44 -0400

    new_hire themeing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  scrollingText: string = 'i thought what i\'d do was, i\'d pretend i was one of those deaf mutes';
+  currTheme: string = 'excel';
+  themeArray: Array<string> = [
+    'excel',
+    'mork',
+  ];
+
+  getCurrTheme() {
+    return this.currTheme;
+  }
+
+  shuffleTheme() {
+    const currThemeIndex = this.themeArray.indexOf(this.currTheme);
+    const maxArrayLength = this.themeArray.length - 1;
+    this.currTheme = currThemeIndex === maxArrayLength ?
+      this.themeArray[0] : this.themeArray[currThemeIndex + 1];
+  }
 }
