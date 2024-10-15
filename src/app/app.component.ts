6fce1ab63f8266a856427e0ac1a005346be46453
Leo Velez <leonardo.velez@kaplan.edu>
2024-10-15 14:57:02 -0400

    new_hire animation test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'corp-borg-builder';
+  scrollingText: string = 'i thought what i\'d do was, i\'d pretend i was one of those deaf mutes';
 }
